0be77ef2ae07cb466cbc29ac99f96219f3b630d9
Joao Claudino <joao.claudinosec@outlook.com>
2025-02-08 10:52:00 -0300

    Start to develop the login function

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from database import db
 from models.user import User
+from flask_login import LoginManager
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Your_Secret_Key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 
-db.init_app(app)
+login_manager = LoginManager()
 
+db.init_app(app)
+login_manager.init_app(app)
+
+
+@app.post("/login")
+def login():
+    data = request.get_json()
+    username = data.get["username"]
+    password = data.get["password"]
+
+    if username and password:
+
+        pass
+
+    return jsonify({"Message":"Invalid Credentials"}), 400
 
 @app.get("/hello-world")
 def hello_world():
