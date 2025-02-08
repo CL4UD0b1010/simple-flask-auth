caacdc51fb0301af150053524b2ee45d2786454f
Joao Claudino <joao.claudinosec@outlook.com>
2025-02-07 21:16:40 -0300

    Adding connection woth db

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "Your_Secret_Key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db = SQLAlchemy(app)
+
+
+@app.get("/hello-world")
+def hello_world():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
